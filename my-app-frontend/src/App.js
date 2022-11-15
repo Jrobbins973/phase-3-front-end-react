ec2b94b02fd572f16f4871e08b0afdd51a4714db
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-11-15 14:10:05 -0500

    trying to add reviews

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -2,11 +2,17 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
 import MovieContainer from './Components/MovieContainer';
+import NavBar from './Components/NavBar';
+import Home from './Components/Home';
+import {Link, Switch, Route} from 'react-router-dom'
+import { Button, Menu} from 'semantic-ui-react'
+import MovieDetails from './Components/MovieDetails';
 
 const baseUrl = 'http://localhost:9292/movies'
 
 function App() {
   const [movies, setMovies] = useState([])
+  const [movieDetails, setMovieDetails] = useState({})
 
   useEffect(() => {
       fetch(baseUrl)
@@ -15,12 +21,56 @@ function App() {
   },[])
   
 
+const getMovie = (movie) => {
+  setMovieDetails(movie)
+}
 
-// console.log(movies)
 
 
-  return (
-    <MovieContainer movies = {movies}/>
+// console.log(movieId)
+
+
+
+// console.log(movieDetails.id)
+// const getMovieDetails = movies.map(movie => key={movie.id}, movie={movie})
+  return ( <div>
+    <Menu>
+      <Link to ='/'>
+        <Button>Home</Button>
+      </Link>
+      <Link to ='/movies'>
+        <Button>Movies</Button>
+      </Link>
+
+
+        <Menu.Item name='upcomingEvents' >
+        Upcoming Events
+        </Menu.Item>
+</Menu>
+
+
+    
+  <Switch>
+      <Route exact path='/'>
+            <Home/>
+      </Route>
+
+
+
+      <Route path='/movies'>
+            <MovieContainer movies = {movies} getMovie= {getMovie}/>
+      </Route>
+
+
+
+        <Route path={`/movie/${movieDetails.id}`}>
+              <MovieDetails movieDetails = {movieDetails}/>
+        </Route>
+  </Switch>
+
+    </div>
+
+
   )
 
 }
--- my-app-frontend/src/Components/MovieContainer.js
+++ my-app-frontend/src/Components/MovieContainer.js
@@ -1,15 +1,29 @@
 import React from "react"
 import MovieCard from "./MovieCard"
+import { Card, Icon, Image, Grid, Menu} from 'semantic-ui-react'
+import {Link, Switch, Route} from 'react-router-dom'
+import MovieDetails from "./MovieDetails"
+
 
 
 
 function MovieContainer(props){
-const {movies} = props
-const renderMovies = movies.map(movie => <MovieCard key={movie.id} movie={movie}/>)
+const {movies, getMovie} = props
+const renderMovies = movies.map(movie => <MovieCard key={movie.id} movie={movie} getMovie={getMovie}/>)
+
 
 return (
+
+
     <div>
+
+
+        <Card.Group itemsPerRow={5}>
         {renderMovies}
+        </Card.Group>
+        
+
+
     </div>
 )
 
